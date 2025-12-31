efc2ccf51a54cc0fae29f974191658bf6111db45
Junius Lekgwara <phutigravel@gmail.com>
2026-01-01 01:24:59 +0200

    removed nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import mdx from "fumadocs-mdx/vite";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -26,6 +25,5 @@ export default defineConfig({
     react(),
     // see https://tanstack.com/start/latest/docs/framework/react/guide/hosting for hosting config
     // we configured nitro by default
-    nitro(),
   ],
 });
